399cdad4f09caa91000c866178e4eb7778acdfb4
Intl Dale <tragicid@gmail.com>
2020-07-30 18:18:31 -0500

    yep

---
+++ main.js
@@ -0,0 +1,16 @@
+let shopping = ['bread', 'milk', 'cheese', 'hummus'];
+//  this is an array
+console.log(shopping);
+
+let spars = ['1', , '123'];
+let random = [123, 'tree', {}];
+
+console.log(random.length);
+
+// loop over an array
+for (var i = 0; i < shopping.length; i++) {
+    console.log('shopping');
+}
+
+// loop over an array
+// shopping.forEach()
